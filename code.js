6d94d733d867f17fe786b48d0590ebcb68848faf
DanYan97 <158104062+DanYan97@users.noreply.github.com>
2024-09-24 15:30:10 +0000

    First Update

--- code.js
+++ code.js
@@ -1,3 +1,17 @@
 function binarySearch(list, element) {
+    let left = 0;
+    let right = list.length - 1;
+
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+
+        if (list[mid] === element) {
+            return mid;
+        } else if (list[mid] < element) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
     return -1;
 }
