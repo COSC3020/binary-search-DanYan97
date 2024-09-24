a4b920385a61717e799a4446c2af26cc2aef5e1b
DanYan97 <158104062+DanYan97@users.noreply.github.com>
2024-09-24 19:52:16 +0000

    Second Update

--- code.js
+++ code.js
@@ -1,17 +1,20 @@
 function binarySearch(list, element) {
     let left = 0;
     let right = list.length - 1;
+    let result=-1;
 
     while (left <= right) {
+
         const mid = Math.floor((left + right) / 2);
 
         if (list[mid] === element) {
-            return mid;
+            result= mid;
+            right=mid-1;
         } else if (list[mid] < element) {
             left = mid + 1;
         } else {
             right = mid - 1;
         }
     }
-    return -1;
+    return result;
 }
